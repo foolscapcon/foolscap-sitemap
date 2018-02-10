2ddda09de153c72b2b9e0f931ed5379e1e059465
Douglas Beal <git@dougbeal.com>
2018-02-10 15:04:50 -0800

    moved js generate to jsonToGraffle.js to use as template

--- foolscap_sitemap/foolscap_sitemap/pipelines.py
+++ foolscap_sitemap/foolscap_sitemap/pipelines.py
@@ -22,59 +22,7 @@ class FoolscapSitemapPipeline(object):
 
 
     def close_spider(self, spider):
-        with open(self.filename, 'w') as output:
-            i = "    "
-            output.write("""var _ = function() {
-            var action = new PlugIn.Action(function(selection){
-            var fitTextFontToShapeSize = this.Functions.fitTextFontToShapeSize;
-            Document.makeNewAndShow(function(doc){
-            cnvs = doc.windows[0].selection.canvas;
-            cnvs.canvasSizingMode = CanvasSizingMode.Fit;
-            cnvs.name = "Foolscap Website";\n\n""")
-
-            output.write("var groups = {};\n")
-            output.write("var all_items = [];\n")
-            output.write("var group_items = [];\n")
-            output.write("var item;\n")
-            for group_url, items in self.groups.items():
-                output.write("group_items = [];\n")
-                for index, link in enumerate(items):
-                    to_url = link['url']
-                    text = link['text']
-                    output.write("item = cnvs.newShape();\n")
-                    output.write("console.log(typeof item);\n")
-                    output.write("console.log(item);\n")
-                    output.write("item.shape = 'RoundRect';\n")
-                    output.write("item.text = '{0}';\n".format(html.escape(text.strip())))
-                    output.write("item.to_url = '{0}';\n".format(to_url))
-
-                    output.write("fitTextFontToShapeSize(item);\n")
-                    output.write("all_items.push(item);\n")
-                    output.write("console.log(item.text);\n")
-                    output.write("console.log(typeof all_items[0]);\n")
-                    output.write("console.log(all_items[0]);\n")
-                    output.write("group_items.push(item);\n")
-                    output.write("\n")
-
-                output.write("groups[\"{group_url}\"] = new Group(group_items);\n".format(**locals()))
-                output.write("console.log(\"group {group_url}\");\n".format(**locals()))
-                break
-            output.write("\n")
-            output.write("console.log(groups);\n")
-            output.write("for (var from_item in all_items) {\n")
-            output.write("    var url = from_item.to_url;\n")
-            output.write("    var to_group = groups[from_item.to_url];\n")
-            output.write("    console.log(url);\n")
-            output.write("    console.log(typeof from_item);\n")
-            output.write("    console.log(from_item);\n")
-            output.write("    console.log(typeof to_group);\n")
-            output.write("    console.log(to_group);\n")
-            output.write("    cnvs.connect(from_item, to_group);\n")
-            output.write("}\n")
-            output.write("});\n")
-            output.write("});//Action\n")
-            output.write("return action;")
-            output.write("""}();\n_;""")
+        pass
 
 
 
